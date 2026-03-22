c3fc0326fa7d13ec4bb785828c41664a375e112c
Enrico <enrico.milanese@whoi.edu>
2026-03-22 14:40:00 -0400

    rename crococamp to model2obs in install files

--- install/envpaths_NCAR.sh
+++ install/envpaths_NCAR.sh
@@ -2,14 +2,14 @@
 export DART_ROOT_PATH="/glade/u/home/emilanese/work/DART-Casper/"
 
 # Define conda environment name
-export CONDA_ENV_NAME="crococamp-dev"
+export CONDA_ENV_NAME="model2obs"
 
 #### DO NOT MODIFY BELOW THIS LINE ####
 # Set up paths
 export CROCOLAKE_OBS_CONV_PATH=${DART_ROOT_PATH%/}/observations/obs_converters/CrocoLake/
 export PYTHONPATH="$CROCOLAKE_OBS_CONV_PATH:\$PYTHONPATH"
 
-export CROCOCAMP_PATH=$(dirname "$PWD")/
+export MODEL2OBS_PATH=$(dirname "$PWD")/
 
 export CROCOLAKE_PATH=$(dirname "$PWD")/CrocoLake/
 export TUTORIAL_DATA_PATH=$(dirname "$PWD")/2025_Crocodile_tutorial/
