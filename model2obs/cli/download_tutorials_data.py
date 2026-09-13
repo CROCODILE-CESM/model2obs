9727116ea2b8d94562fedb9daf9c189521f6d058
Enrico <enrico.milanese@whoi.edu>
2026-09-13 17:52:10 -0400

    add timeout to download request

--- model2obs/cli/download_tutorials_data.py
+++ model2obs/cli/download_tutorials_data.py
@@ -74,7 +74,7 @@ def download_file(url, local_filename):
     local_filename (str)  --  Local filename to save the file to.
     """
     try:
-        with requests.get(url, stream=True) as r:
+        with requests.get(url, timeout=60, stream=True) as r:
             r.raise_for_status()
             total_size = int(r.headers.get('content-length', 0))
             with open(local_filename, "wb") as f, tqdm(
