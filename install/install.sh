bf3a2cf8931f2a12b68b44f77f70ace8787013f3
Enrico <enrico.milanese@whoi.edu>
2026-09-13 12:04:15 -0400

    replace mamba with conda

--- install/install.sh
+++ install/install.sh
@@ -12,7 +12,7 @@ done
 
 ## create conda environment with name set in envpaths.sh
 source ./envpaths.sh
-mamba env create --name "$CONDA_ENV_NAME" -f ../environment.yml -y
+conda env create --name "$CONDA_ENV_NAME" -f ../environment.yml -y
 CONDA_ENV_PATH=$(conda env list | awk -v env="$CONDA_ENV_NAME" '$1 == env { print $NF }')
 
 ## scripts to run when environment is activated
